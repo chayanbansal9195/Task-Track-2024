ce26ff1e6da9812d226dc7d7b5f5ffc7fb59c584
Chayan Bansal <chayanbansal1999@gmail.com>
2024-05-26 20:35:44 +0530

    adding style for form component

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./TaskForm.css";
 
 const TaskForm = () => {
   return (
@@ -10,19 +11,23 @@ const TaskForm = () => {
           placeholder="Enter your task"
         />
         <div className="task_form_bottom_line">
-          <button className="tag">HTML</button>
-          <button className="tag">CSS</button>
-          <button className="tag">Javascript</button>
-          <button className="tag">React</button>
+          <div>
+            <button className="tag">HTML</button>
+            <button className="tag">CSS</button>
+            <button className="tag">Javascript</button>
+            <button className="tag">React</button>
+          </div>
 
-          <select className="task_status">
-            <option value="todo">Todo</option>
-            <option value="doing">Doing</option>
-            <option value="done">Done</option>
-          </select>
-          <button type="submit" className="task_submit">
-            + Add Task
-          </button>
+          <div>
+            <select className="task_status">
+              <option value="todo">Todo</option>
+              <option value="doing">Doing</option>
+              <option value="done">Done</option>
+            </select>
+            <button type="submit" className="task_submit">
+              + Add Task
+            </button>
+          </div>
         </div>
       </form>
     </header>
